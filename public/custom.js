8dffc897d5b2378dff1adc67f51317af87e4274b
melcanz08 <melcanz1985m@gmail.com>
2023-09-20 21:38:07 +0800

    second commit

---
+++ public/custom.js
@@ -0,0 +1,24 @@
+const socket = new WebSocket('ws://localhost:3000');
+let msgDiv = document.getElementById("msgDiv");
+socket.onopen = ()=>{
+	console.log("Websocket connected to the server!")
+};
+
+socket.onmessage = (event)=>{
+	console.log("Message received from server: ", event.data);
+	let msgContent = document.createElement("div");
+	msgContent.classList.add("my-3");
+	msgContent.textContent = event.data;
+	msgDiv.appendChild(msgContent);
+};
+
+socket.onclose = ()=>{
+
+};
+
+let btnSend = document.getElementById("btnSend");
+let inputField = document.querySelector("[name='name']");
+
+btnSend.onclick = ()=>{
+	socket.send(inputField.value);
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express()
+const WebSocket = require("ws")
+
+const PORT = process.env.PORT || 3000
+
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+
+app.get("/favicon.ico", (req, res)=> res.status(204))
+
+app.get("/", (req, res)=>{
+	console.log(req.url)
+	res.render("index")
+})
+
+const server = app.listen(PORT, ()=>{
+	console.log("Server live on port:", PORT)
+})
+
+const websocket = new WebSocket.WebSocketServer({ server })
+
+websocket.on("connection", (socket)=>{
+	console.log("New client connected on the server!")
+
+	socket.on("message", (msg)=>{
+		console.log("Messsage received from client: ",msg.toString())
+		socket.send(msg.toString())
+	})
+
+
+})
+
